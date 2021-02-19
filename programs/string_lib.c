0af063e35686e9330dac55566c6ee887bde15437
rajpal-se <67093654+rajpal-se@users.noreply.github.com>
2021-02-19 21:41:34 +0530

    Update string_lib.c

--- programs/string_lib.c
+++ programs/string_lib.c
@@ -1,4 +1,6 @@
-// Program
+//      strcmp
+// Compares the C string str1 to the C string str2.
+
 #include <stdio.h>
 #include <string.h>
 
@@ -24,3 +26,38 @@ int main (){
     compare("abc", "abcd"); // S1 < S2
     return 0;
 }
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------*/
+
+//      strncmp
+// Compares the C string str1 to the C string str2 upto <num> charactors.
+
+#include <stdio.h>
+#include <string.h>
+
+void compare(char *s1, char *s2){
+    int i = strncmp(s1, s2);
+    if(i > 0)
+        printf("S1 > S2");
+    else if(i < 0)
+        printf("S1 < S2");
+    else
+        printf("S1 = S2");
+    printf("\n");
+}
+
+int main (){
+    char a[] = {'a', 'b', 'c', '\0'};
+    compare(a, "abc");      // S1 = S2
+    compare("abc", "abc");  // S1 = S2
+    compare("bbc", "abc");  // S1 > S2
+    compare("abc", "bbc");  // S1 < S2
+    compare("aBc", "abc");  // S1 < S2
+    compare("abcd", "abc"); // S1 = S2
+    compare("abc", "abcd"); // S1 = S2
+    return 0;
+}
+
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------*/
+
