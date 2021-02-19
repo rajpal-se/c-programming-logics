fc9cb9c887a0117c775a3936494821cc3f9c38ca
rajpal-se <67093654+rajpal-se@users.noreply.github.com>
2021-02-19 21:46:04 +0530

    Create strncmp.c

---
+++ programs/string/strncmp.c
@@ -0,0 +1,28 @@
+//      strncmp
+// Compares the C string str1 to the C string str2 upto <num> charactors.
+
+#include <stdio.h>
+#include <string.h>
+
+void compare(char *s1, char *s2){
+    int i = strncmp(s1, s2);
+    if(i > 0)
+        printf("S1 > S2");
+    else if(i < 0)
+        printf("S1 < S2");
+    else
+        printf("S1 = S2");
+    printf("\n");
+}
+
+int main (){
+    char a[] = {'a', 'b', 'c', '\0'};
+    compare(a, "abc");      // S1 = S2
+    compare("abc", "abc");  // S1 = S2
+    compare("bbc", "abc");  // S1 > S2
+    compare("abc", "bbc");  // S1 < S2
+    compare("aBc", "abc");  // S1 < S2
+    compare("abcd", "abc"); // S1 = S2
+    compare("abc", "abcd"); // S1 = S2
+    return 0;
+}
